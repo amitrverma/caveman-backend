562070540d0782c611337af0f94bcc6ed9a964fa
Amit R Verma <amit@amitrverma.com>
2025-06-06 16:54:33 +0530

    some change

--- app/database.py
+++ app/database.py
@@ -2,11 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config import settings
 
-engine = create_async_engine(
-    settings.DATABASE_URL,
-    echo=True,
-    connect_args={"ssl": True}  # ✅ THIS is required for asyncpg with Supabase
-)
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
 
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
